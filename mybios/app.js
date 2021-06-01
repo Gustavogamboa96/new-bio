368a5d40edd56d115e416e3cac6acbb5bbb8f6e0
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2021-06-01 09:34:52 -0400

    No tokens

--- mybios/app.js
+++ mybios/app.js
@@ -7,10 +7,10 @@ const serverless = require('serverless-http');
 
 
 const client = new Twit({
-    consumer_key: 'BYFev5CvRdyx7EgOQpLKEwTLG',
-    consumer_secret: 'dkDkC4NSRTTAjcRJaGeD9hlXfTJOPFBiAIhvEdaSTJZKTtGnJp',
-    access_token: '51788921-CMYMxgZ4TvOP7OjXTqC8GsjULjn0hfjzCsifAkHCj',
-    access_token_secret: 'pwImFpNUQr86ar85bX2azpnvLgwWENqYkmMDQ2iEcJGvs',
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
     //timeout_ms: 60*1000,
 })
 
